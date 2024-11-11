e34c1ee36736c019a812d0485b2653facff2af80
agungdhewe <agung_dhewe@yahoo.com>
2024-11-11 11:42:40 +0700

    fix plaintemplate

--- src/PlainTemplate.php
+++ src/PlainTemplate.php
@@ -2,21 +2,28 @@
 
 
 class PlainTemplate extends WebTemplate {
-	const string NAME = "plaintemplate";
+	const string DEFAULT_NAME = "plaintemplate";
 
 	private ?string $curr_tpldir;
+	private ?string $curr_templatename;
 
-	function __construct(?string $tpldir=null) {
+	function __construct(?string $tpldir=null, ?string $templatename=null) {
 		$this->curr_tpldir = $tpldir;
+		$this->curr_templatename = $templatename;
+
 	}
 
 	public function GetName() : string {
-		return self::NAME;
+		if ($this->curr_templatename!=null) {
+			return $this->curr_templatename;
+		} else {
+			return self::DEFAULT_NAME;
+		}
 	}
 
 	public function GetTemplateDir() : string {
+		$name = $this->GetName();
 		if ($this->curr_tpldir==null) {
-			$name = $this->GetName();
 			$templatedir = implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'templates', $name]);
 			return $templatedir;
 		} else {
