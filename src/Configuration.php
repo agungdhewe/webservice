aca2b4cb17e8868584516b3ff0c5db00e6bda29b
agungdhewe <agung_dhewe@yahoo.com>
2024-11-26 08:48:41 +0700

    change Configuration using agungdhewe/setingan

--- src/Configuration.php
+++ src/Configuration.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 namespace AgungDhewe\Webservice;
 
+use AgungDhewe\Setingan\Config;
+
 use AgungDhewe\PhpLogger\Log;
 use AgungDhewe\PhpLogger\Logger;
 use AgungDhewe\PhpLogger\LoggerOutput;
@@ -10,9 +12,8 @@ define('HOURS', 3600);
 define('MINUTES', 60);
 
 
-final class Configuration
+final class Configuration extends Config
 {
-	const string SPARATOR = ".";
 	const string DB_MAIN = "DbMain";	
 
 	const string DEBUG_CHANNEL_NAME = 'webservice-debug-channel';
@@ -22,69 +23,6 @@ final class Configuration
 		\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
 		\PDO::ATTR_PERSISTENT=>true
 	];
-
-	private static array $_config;
-	private static array $_usedConfig;
-	private static string $_rootDir;
-
-	public static function Setup(array $config) : void {
-		self::$_config = $config;
-	}
-
-	public static function Set(string $keypath, mixed $value) : void {
-	}
-
-	public static function Get(?string $keypath = null) : mixed {
-		try {
-			if ($keypath!==null) {
-				$value = self::getValueByPath(self::$_config, $keypath);
-				return $value;
-			} else {
-				return self::$_config;
-			}
-		} catch (\Exception $ex) {
-			throw $ex;
-		}
-	}
-
-
-	public static function GetUsedConfig(string $name) : string {
-		if (!array_key_exists($name, self::$_usedConfig)) {
-			throw new \Exception("Config '$name' tidak ditemukan");
-		}
-		return self::$_usedConfig[$name];
-	}
-
-	public static function UseConfig(array $usedconfig) : void {
-		self::$_usedConfig = $usedconfig;
-	}
-
-	private static function getValueByPath(array $array, string $path, ?string $separator = self::SPARATOR) : mixed {
-		$keys = explode($separator, $path);
-		foreach ($keys as $key) {
-			if (!isset($array[$key])) {
-				return null; // Kunci tidak ditemukan
-			}
-			$array = $array[$key]; // Melangkah lebih dalam ke array
-		}
-		return $array;
-	}
-
-
-	public static function setRootDir($dir) : void {
-		if (!defined('__ROOT_DIR__')) {
-			define('__ROOT_DIR__', $dir);
-		}
-
-		self::$_rootDir = $dir;
-
-
-		
-	}
-
-	public static function getRootDir() : string {
-		return self::$_rootDir;
-	}
 
 
 	public static function setLogger() : void {
